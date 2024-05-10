0b0d9af21d94fbdfde5071a8394c732d469c3f9f
Myron R Byrnes <myronbyrnes@gmail.com>
2024-05-10 15:17:02 -0400

    Scanner Special Keywords

---
+++ src/Expr.java
@@ -0,0 +1,135 @@
+abstract class Expr {
+  interface Visitor<R> {
+    R visitBinaryExpr(Binary expr);
+    R visitGroupingExpr(Grouping expr);
+    R visitLiteralExpr(Literal expr);
+    R visitThisExpr(This expr);
+    R visitUnaryExpr(Unary expr);
+    R visitVariableExpr(Variable expr);
+  }
+
+  // Nested Expr classes here...
+//> expr-assign
+  static class Assign extends Expr {
+    Assign(Token name, Expr value) {
+      this.name = name;
+      this.value = value;
+    }
+
+    final Token name;
+    final Expr value;
+    @Override
+    <R> R accept(Expr.Visitor<R> visitor) {
+      // TODO Auto-generated method stub
+      throw new UnsupportedOperationException("Unimplemented method 'accept'");
+    }
+  }
+//< expr-assign
+//> expr-binary
+  static class Binary extends Expr {
+    Binary(Expr left, Token operator, Expr right) {
+      this.left = left;
+      this.operator = operator;
+      this.right = right;
+    }
+
+    @Override
+    <R> R accept(Visitor<R> visitor) {
+      return visitor.visitBinaryExpr(this);
+    }
+
+    final Expr left;
+    final Token operator;
+    final Expr right;
+  }
+//< expr-binary
+//> expr-call
+
+//< expr-call
+
+//< expr-get
+//> expr-grouping
+  static class Grouping extends Expr {
+    Grouping(Expr expression) {
+      this.expression = expression;
+    }
+
+    @Override
+    <R> R accept(Visitor<R> visitor) {
+      return visitor.visitGroupingExpr(this);
+    }
+
+    final Expr expression;
+  }
+//< expr-grouping
+//> expr-literal
+  static class Literal extends Expr {
+    Literal(Object value) {
+      this.value = value;
+    }
+
+    @Override
+    <R> R accept(Visitor<R> visitor) {
+      return visitor.visitLiteralExpr(this);
+    }
+
+    final Object value;
+  }
+//< expr-literal
+//> expr-logical
+
+//< expr-logical
+//> expr-set
+
+//< expr-set
+//> expr-super
+
+//< expr-super
+//> expr-this
+  static class This extends Expr {
+    This(Token keyword) {
+      this.keyword = keyword;
+    }
+
+    @Override
+    <R> R accept(Visitor<R> visitor) {
+      return visitor.visitThisExpr(this);
+    }
+
+    final Token keyword;
+  }
+//< expr-this
+//> expr-unary
+  static class Unary extends Expr {
+    Unary(Token operator, Expr right) {
+      this.operator = operator;
+      this.right = right;
+    }
+
+    @Override
+    <R> R accept(Visitor<R> visitor) {
+      return visitor.visitUnaryExpr(this);
+    }
+
+    final Token operator;
+    final Expr right;
+  }
+//< expr-unary
+//> expr-variable
+  static class Variable extends Expr {
+    Variable(Token name) {
+      this.name = name;
+    }
+
+    @Override
+    <R> R accept(Visitor<R> visitor) {
+      return visitor.visitVariableExpr(this);
+    }
+
+    final Token name;
+  }
+//< expr-variable
+
+  abstract <R> R accept(Visitor<R> visitor);
+}
+//< Appendix II expr
